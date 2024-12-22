6a871c64d5fe39a0f62826375b94dc5ea6fe76d6
ben509-bo <c12386791@gmail.com>
2024-12-22 06:39:04 -0500

    Update set.js

--- set.js
+++ set.js
@@ -10,8 +10,8 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
 module.exports = { session: process.env.SESSION_ID || 'zokk',
      ETAT:process.env.ETAT,
     PREFIXE: process.env.PREFIXE,
-    NOM_OWNER: process.env.NOM_OWNER || "Zokou-Md",
-    NUMERO_OWNER : process.env.NUMERO_OWNER,              
+    NOM_OWNER: process.env.NOM_OWNER || "carl",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "50941610347"           
     LECTURE_AUTO_STATUS: process.env.LECTURE_AUTO_STATUS || "non",
     TELECHARGER_AUTO_STATUS: process.env.TELECHARGER_AUTO_STATUS || 'non',
     MODE: process.env.MODE_PUBLIC,
